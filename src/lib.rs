10af58b61a5988fc5ea682bfd8dc0060293639ca
Michael Sheely <msheely@hmc.edu>
2016-12-05 15:58:54 -0800

    Worked on API and trying to compile with unused type parameters

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,57 @@
-extern crate petgraph;
-use petgraph::Graph;
+/// The Michael Jackson Graph API
+///
+/// ## Examples
+/// ```rust
+/// use michael_jackson::Graph;
+/// let x = Graph::new();
+/// ```
 
+use std::marker::PhantomData;
+
+pub struct Graph<V, E> {
+    //phantom: PhantomData<(V, E)>,
+}
+pub struct Vertex<V> {
+    //value: PhantomData<V>,
+}
+pub struct Edge<V, E> {
+    /*
+    weight: PhantomData<E>,
+    parent: PhantomData<V>,
+    child: PhantomData<V>,
+    */
+}
+
+// Directed/Undirected vs Just undirected
+// Add nodes and edges
+// Delete nodes and edges
+// Modify weights/values
+// get Neighbors of a node
+// edges can be pattern matched to get endpoints
+// edge and vertex contraction (not featured in petgraph)
+// test for adjacency
+// size()
+// get_adjacency_matrix()
+// get_laplacian(), number_connected_components()
+
+impl<V, E> Graph<V, E> {
+    /// Create a new, empty graph
+    pub fn new() -> Self {
+        Graph {}
+        //Graph { phantom: default() }
+    }
+    /// Construct a graph without data, just for its topology
+    pub fn new_from_edges(edges: Vec<(u32, u32)>) -> Self {
+        //Graph { phantom: default() }
+        Graph {}
+    }
+}
+
+/*
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
     }
 }
+*/
