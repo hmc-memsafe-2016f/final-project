8ac1f24f04c6ef066ad5d6fe0e297e9adcd4f82e
rmawhorter <rmawhorter@g.hmc.edu>
2016-12-13 20:56:10 -0800

    limited jraphics commit

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,54 @@
 //Just a place to store implementation details for the actual game part.
 
+extern crate sfml;
+
+use sfml::graphics::{PrimitiveType, CircleShape, Color, Drawable, RectangleShape, RenderStates, RenderTarget,
+                     RenderWindow, Shape, Transformable};
+use sfml::window::{Key, VideoMode, Event, window_style};
+use sfml::system::Vector2f;
+
+pub struct Point<'s>
+{
+	pos: Vector2f,
+	shape: CircleShape<'s>
+}
+
+impl<'s> Point<'s>
+{
+	pub fn new(x: f32, y: f32) -> Self
+	{
+		pos = Vector2f::new(x, y);
+		let mut shape = CircleShape::new_init(10f32 , 50);
+		shape.set_position2f(x, y);
+		shape.set_fill_color(&Color::red());
+
+		Point
+		{
+			pos:	pos,
+			shape:	shape,
+		}
+	}
+}
+
+impl<'s> Drawable for Point<'s>
+{
+	fn draw(&self, render_target: &mut RenderTarget, _: &mut RenderStates) {
+        render_target.draw(&self.shape);
+    }
+}
+
+/*
+pub struct Line
+{
+	p1: Vector2f,
+	p2: Vector2f,
+	//shape: line
+}
+*/
+
+
+
+/*
 //For two line segments defined by points,
 //when do they intersect?
 //TODO: CLEAN THIS UP
@@ -41,3 +90,4 @@ fn lines_intersect(line1_p1: (i32, i32), line1_p2: (i32, i32), line2_p1: (i32, i
 		}
 	}
 }
+*/
