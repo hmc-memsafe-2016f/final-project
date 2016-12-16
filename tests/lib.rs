d04d84f14675af6d01fe646bcc4e3a6f9686d353
lviornery <lviornery@hmc.edu>
2016-12-15 20:24:11 -0800

    OR DID WE?

--- tests/lib.rs
+++ tests/lib.rs
@@ -28,3 +28,11 @@ fn createnode() {
     let anode = g.create_node(10);
     assert!(g.map_node(&anode, |x| {*x==10}).unwrap(), true)
 }
+
+// This attribute identifies this function as a unit test
+#[test]
+fn createnode() {
+    let mut g = Graph::<i64, i64>::new();
+    let anode = g.create_node(10);
+    assert!(g.map_node(&anode, |x| {*x==10}).unwrap(), true)
+}
