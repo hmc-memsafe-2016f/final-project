6d10c5039fcc2b6a265aa0979db616a051a8a8f9
rmawhorter <rmawhorter@g.hmc.edu>
2016-12-15 20:36:24 -0800

    yes

--- tests/lib.rs
+++ tests/lib.rs
@@ -36,3 +36,18 @@ fn createnode() {
     let anode = g.create_node(10);
     assert!(g.map_node(&anode, |x| {*x==10}).unwrap(), true)
 }
+
+fn getnodes() {
+    let mut g = Graph::<i64, i64>::new();
+    let a_node = g.create_node(10);
+    let b_node = g.create_node(10);
+    let c_node = g.create_node(10);
+    g.create_edge(a_node, b_node, 5);
+    g.create_edge(a_node, c_node, 5);
+
+    for node in g.nodes()
+    {
+    	assert!(g.map_node(&node, |x| {*x==10}).unwrap(), true)
+    }
+}
+
